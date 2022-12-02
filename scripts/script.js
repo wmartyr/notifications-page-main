129249c7cf4174d43efb9f4a6bad9f0ce7b130ba
wmartyr <woodrow.martyr@gmail.com>
2022-12-02 17:12:26 +1030

    Code button to mark all as read

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,25 @@
 let unreadCount = 3;
 
+const readButton = document.querySelector("button");
+const unreadCountDisplay = document.querySelector(".unread-count");
+
+unreadCountDisplay.textContent = unreadCount;
+
+
+readButton.addEventListener("click", () => {
+    const cards = document.querySelectorAll(".card");
+    const dots = document.querySelectorAll(".unread-dot");
+    let count = 0;
+
+    for (let i = 0; i <= unreadCount; i++) {
+        cards[i].classList.remove("unread");
+    }
+
+    dots.forEach((dot) => {
+        dot.remove();
+    })
+
+    unreadCount = 0;
+    unreadCountDisplay.textContent = unreadCount;
+
+});
